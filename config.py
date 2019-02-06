3886a48b24e61e95f481532333e0f856f929940f
halkano <halkanoh10@gmaile.com>
2019-02-06 04:32:47 +0300

    created an api key

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
 import os
-cl
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLE_API_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_API_URL =os.environ.get('SECRET_API_KEY')
+
+
+
+class ProdConfig(Config):
+    '''
     Production  configuration child class
 
     Args:
