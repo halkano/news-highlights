564b57ee6f86ca682a188d46a97eb83b21fda740
halkano <halkanoh10@gmaile.com>
2019-02-06 04:31:12 +0300

    added config.py and its content

--- config.py
+++ config.py
@@ -1,18 +1,5 @@
 import os
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
-    ARTICLE_API_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_API_URL =os.environ.get('SECRET_API_KEY')
-
-
-
-class ProdConfig(Config):
-    '''
+cl
     Production  configuration child class
 
     Args:
